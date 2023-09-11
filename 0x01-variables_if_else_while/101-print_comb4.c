cae1a35b6ea2be8a5c59412d7a4010c6386d64e2
alv0br0wn <alvinbrown660@outlook.com>
2023-09-11 21:42:17 +0300

    success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *Description: 'print combinations of 3digits'
+  *Description: 'print different combinations of 3 digits'
   *Return : Always o
   */
 int main(void)
 {
 	int digit1, digit2, digit3;
+
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
