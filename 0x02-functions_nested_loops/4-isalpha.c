2b3a35be5d72efb68d3bf865898483f8705364e1
alv0br0wn <alvinbrown660@outlook.com>
2023-09-12 18:44:08 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,13 @@
  */
 int _isalpha(int c)
 {
-	if (islower(c) || isupper(c))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	_putchar('\n');
+}
