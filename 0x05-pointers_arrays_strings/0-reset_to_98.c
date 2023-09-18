02a3701b60e3cc53363580d7e97011d8896a88cf
alv0br0wn <alvinbrown660@outlook.com>
2023-09-18 19:22:19 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * reset_to_98 - takea spointer to int parameter
+  *		and updates the value it points to 98
+  *
+  * @n: int parameter
+  *
+Return: Always(0)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
