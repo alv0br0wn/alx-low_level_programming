0e8d019b9e51dd0a8b10f7c07584fd04afc311f9
alv0br0wn <141228393+alv0br0wn@users.noreply.github.com>
2023-09-21 17:45:58 +0300

    srcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,13 @@
 /**
-   * _strcat - function that concatenates
-    *          two strings.
-     *
-      * @dest: pointer to destnation input
-       * @src: pointer to source input
-        *
-	 * Return: pointer to resulting string @dest
-	 */
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
@@ -17,7 +18,9 @@ char *_strcat(char *dest, char *src)
 		c++;
 
 	/* iterate through each src array value without the null byte*/
-	dest[c++] = src[c2];
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
 
 	return (dest);
 }
